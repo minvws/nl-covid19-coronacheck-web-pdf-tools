1e81b01096cb5344a4286c8d546dbe8c2623bd31
Jeroen Breen <jeroen@innouveau.nl>
2021-06-29 11:26:48 +0200

    add territory for generateQR

--- src/qr.js
+++ src/qr.js
@@ -7,15 +7,16 @@ const dpi = 300;
 /**
  * @param {string} qrCode
  * @param {number} sizeInCm
+ * @param {string} territory
  * @return {Promise<string>}
  */
-export const generateQR = (qrCode, sizeInCm) => {
-  const sizeInPixels = Math.round(sizeInCm * cmToInch * dpi);
-  const qrOptions = {
-    width: sizeInPixels,
-    height: sizeInPixels,
-    margin: 0,
-    errorCorrectionLevel: "L",
-  };
-  return qrcode.toDataURL(qrCode, qrOptions);
+export const generateQR = (qrCode, sizeInCm, territory) => {
+    const sizeInPixels = Math.round(sizeInCm * cmToInch * dpi);
+    const qrOptions = {
+        width: sizeInPixels,
+        height: sizeInPixels,
+        margin: 0,
+        errorCorrectionLevel: territory === "nl" ? "L" : "Q",
+    };
+    return qrcode.toDataURL(qrCode, qrOptions);
 };
