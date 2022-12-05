27a08a28b2b2ac7249e9529cd0f459fa04911bea
Mario Nujic <97021674+LogisticsCare@users.noreply.github.com>
2022-12-05 16:38:07 +0100

    Updated ckvp cover page contact information text (#139)

--- dist/index.js
+++ dist/index.js
@@ -16539,7 +16539,7 @@ function structLetterBody(doc, args) {
       }], lineSpace / 4);
     }
     heading("Heeft u nog vragen over corona?");
-    paragraph("Bel ons via 0800 - 1351. Dit nummer is iedere dag open van 08.00 tot 20.00 uur. Wij helpen u graag verder.");
+    paragraph("Heeft u nog vragen over corona?Bel ons via 0800 - 1421. Dit nummer is iedere werkdag open van 08.00 tot 18.00 uur. Wij helpen u graag verder");
   });
 }
 
